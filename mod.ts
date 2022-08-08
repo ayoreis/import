1bee6c15d4b2674646f7d947915363274eb3d0d4
Ayo Reis <84969551+ayoreis@users.noreply.github.com>
2022-08-08 19:55:56 +0100

    feat(ponyfill): Strip shebang

--- mod.ts
+++ mod.ts
@@ -1,14 +1,15 @@
 import * as esbuild from 'https://deno.land/x/esbuild@v0.14.53/wasm.js'
 import { denoPlugin } from 'https://deno.land/x/esbuild_deno_loader@0.5.2/mod.ts'
+import stripShebang from 'https://esm.sh/strip-shebang@2.0.0'
 
 const AsyncFunction = (async function () {}).constructor
 
 export async function importModule(
     moduleName: string
 ): Promise<Record<'default' | string, any>> {
-    try {
-        return await import(moduleName)
-    } catch {
+    // try {
+    //     return await import(moduleName)
+    // } catch {
         esbuild.initialize({ worker: false })
 
         const result = await esbuild.build({
@@ -26,11 +27,10 @@ export async function importModule(
         const [ before, after ] = text.split('export {')
         
         const body =
-            before
+            stripShebang(before)
             + (after
             ? 'return {' + after.replaceAll(/(\w+) (as) (\w+)/gi, '$3: $1')
             : '')
-
+            
         return AsyncFunction(body)()
-    }
 }
