d46d08353cc731478e72b36abd528629e646bb46
Ayo Reis <84969551+ayoreis@users.noreply.github.com>
2022-08-09 15:32:35 +0100

    feat: Module evaluator

--- mod.ts
+++ mod.ts
@@ -1,14 +1,49 @@
 import * as esbuild from 'https://deno.land/x/esbuild@v0.14.53/wasm.js'
 import { denoPlugin } from 'https://deno.land/x/esbuild_deno_loader@0.5.2/mod.ts'
 import stripShebang from 'https://esm.sh/strip-shebang@2.0.0'
+import { encode } from 'https://deno.land/std@0.151.0/encoding/base64url.ts'
+import { toFileUrl } from 'https://deno.land/std@0.151.0/path/mod.ts'
+
+export type Module = Promise<Record<'default' | string, any>>
 
 const AsyncFunction = (async function () {}).constructor
 
-export async function importModule(
-    moduleName: string
-): Promise<Record<'default' | string, any>> {
+function moduleToAsyncFunction(moduleString: string): Module {
+    const [ before, after ] = moduleString.split('export {')
+          
+    const body =
+        stripShebang(before)
+        + (after
+        ? 'return {' + after.replaceAll(/(\w+) (as) (\w+)/gi, '$3: $1')
+        : '')
+        
+    return AsyncFunction(body)()
+}
+
+export async function importModule(moduleName: string): Module {
     try {
         return await import(moduleName)
+    } catch {
+        esbuild.initialize({ worker: false })
+
+        const result = await esbuild.build({
+            bundle: true,
+            entryPoints: [ import.meta.resolve(moduleName) ],
+            plugins: [ denoPlugin() ],
+            write: false,
+            logLevel: 'silent',
+            format: 'esm',
+        })
+
+        esbuild.stop()
+
+        return moduleToAsyncFunction(result.outputFiles[ 0 ].text)
+    }
+}
+
+export async function importString(moduleString: string): Module {
+    try {
+        return await import(`data:text/tsx;base64,${ encode(moduleString) }`)
     } catch {
         esbuild.initialize({ worker: false })
 
@@ -18,20 +53,15 @@ export async function importModule(
             write: false,
             logLevel: 'silent',
             format: 'esm',
-            entryPoints: [ import.meta.resolve(moduleName) ],
+            stdin: {
+                contents: moduleString,
+                loader: 'tsx',
+                sourcefile: toFileUrl(Deno.cwd()).href
+            },
         })
 
         esbuild.stop()
 
-        const { text } = result.outputFiles[ 0 ] 
-        const [ before, after ] = text.split('export {')
-        
-        const body =
-            stripShebang(before)
-            + (after
-            ? 'return {' + after.replaceAll(/(\w+) (as) (\w+)/gi, '$3: $1')
-            : '')
-            
-        return AsyncFunction(body)()
+        return moduleToAsyncFunction(result.outputFiles[ 0 ].text)
     }
 }
